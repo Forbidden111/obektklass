3d9592e78744122c7832b911097ca0ad66bacd7a
Dr.Abdulagaev <magaram.1995@mail.ru>
2023-08-07 19:19:34 +0300

    domashka sdelana 2

--- src/obektklasss/Book.java
+++ src/obektklasss/Book.java
@@ -1,5 +1,7 @@
 package obektklasss;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
 
@@ -26,5 +28,27 @@ public class Book {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && Objects.equals(autor, book.autor);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", autor=" + autor +
+                ", year=" + year +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, autor, year);
     }
 }
